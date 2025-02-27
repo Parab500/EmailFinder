fe987c3f7cecafcf12c8beb3ac9ecaa0b1ad6b11
Parab500 <59521004+Parab500@users.noreply.github.com>
2025-02-27 11:44:04 +0530

    Update email_parser.py

--- emailfinder/utils/file/email_parser.py
+++ emailfinder/utils/file/email_parser.py
@@ -1,10 +1,9 @@
 import re
 
-
 def get_emails(target, text):
-    regex = r"[a-zA-Z0-9_\.+-]+@" + target
-    emails = re.findall(regex, text.replace("<em>", "").replace("<\em>","")
-                                .replace("<strong>", "").replace("</strong>", "")
-                                .replace("<b>", "").replace("</b>", ""))
+    regex = r"[a-zA-Z0-9_\.+-]+@" + re.escape(target)  # Escape target to prevent regex issues
+    cleaned_text = text.replace("<em>", "").replace("</em>", "") \
+                       .replace("<strong>", "").replace("</strong>", "") \
+                       .replace("<b>", "").replace("</b>", "")
+    emails = re.findall(regex, cleaned_text)
     return emails
-
